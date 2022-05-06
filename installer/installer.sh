2690e427a8965782e9a039ff6c797c1c3d39b821
SpieringsAE <91117176+SpieringsAE@users.noreply.github.com>
2022-05-06 13:28:06 +0200

    removed the commit script from the installer
    remove hostapd

--- installer/installer.sh
+++ installer/installer.sh
@@ -6,7 +6,6 @@
 echo -e "download the service and other neccesary scripts"
 git clone https://github.com/SpieringsAE/controller_scripts.git --branch $1
 
-cp -arp /root/controller_scripts/etc /
 cp -arp /root/controller_scripts/lib /
 cp -arp /root/controller_scripts/usr /
 
@@ -16,3 +15,4 @@ apt install rfkill -y
 
 apt install python3-pip -y
 
+apt remove hostapd -y
